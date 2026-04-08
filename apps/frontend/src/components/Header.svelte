3f8855cf82648594c17865ff9ed2ea8bdb625ed3
Yiyoung Liu <yiyoung.md@gmail.com>
2026-04-08 00:21:52 -0400

    feat: add header

---
+++ apps/frontend/src/components/Header.svelte
@@ -0,0 +1,3 @@
+<header class="min-h-20 bg-gray shadow-md">
+	<div>Hi</div>
+</header>
--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,3 +1,8 @@
+<script lang="ts">
+	import Header from '../components/Header.svelte';
+</script>
+
+<Header />
 <main>
 	<h1>CMU Courses</h1>
 </main>
