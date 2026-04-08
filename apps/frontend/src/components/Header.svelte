17793502abd4d65959d8227e007cc167c6449696
Yiyoung Liu <yiyoung.md@gmail.com>
2026-04-08 00:57:15 -0400

    feat: improve header css

--- apps/frontend/src/components/Header.svelte
+++ apps/frontend/src/components/Header.svelte
@@ -2,11 +2,13 @@
 	import favicon from '@/lib/assets/favicon.ico';
 </script>
 
-<header class="flex min-h-20 items-center bg-gray shadow-md">
-	<a href="/">
-		<div class="flex items-center">
-			<img src={favicon} class="h-8 w-8 rounded" alt="favicon" />
-			<span class="ml-2">CMU Courses</span>
-		</div>
-	</a>
+<header class="flex h-16 flex-row items-center justify-between bg-gray p-6 shadow-md">
+	<div class="text-dark-gray-500 flex flex-initial cursor-pointer flex-row justify-start">
+		<a href="/">
+			<div class="flex items-center">
+				<img src={favicon} class="h-8 w-8 rounded" alt="favicon" />
+				<h6 class="ml-2 font-medium text-dark-gray">CMU Courses</h6>
+			</div>
+		</a>
+	</div>
 </header>
