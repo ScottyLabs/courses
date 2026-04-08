4ba56377694e358f3947280bf06617318b44c273
Yiyoung Liu <yiyoung.md@gmail.com>
2026-04-08 01:24:20 -0400

    fix: redirection issue?

--- apps/frontend/src/lib/components/Header.svelte
+++ apps/frontend/src/lib/components/Header.svelte
@@ -3,12 +3,10 @@
 </script>
 
 <header class="flex h-16 flex-row items-center justify-between bg-gray p-6 shadow-md">
-	<div class="text-dark-gray-500 flex flex-initial cursor-pointer flex-row justify-start">
-		<a href="/">
-			<div class="flex items-center">
-				<img src={favicon} class="h-8 w-8 rounded" alt="favicon" />
-				<h6 class="ml-2 font-medium text-dark-gray">CMU Courses</h6>
-			</div>
-		</a>
-	</div>
+	<a href="/" class="text-dark-gray-500 flex flex-initial flex-row justify-start hover:underline">
+		<div class="flex items-center">
+			<img src={favicon} class="h-8 w-8 rounded" alt="favicon" />
+			<h6 class="ml-2 font-medium text-dark-gray">CMU Courses</h6>
+		</div>
+	</a>
 </header>
