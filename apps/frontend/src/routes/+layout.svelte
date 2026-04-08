53da8872f7e657072a9e14aaf705090b54ca3698
Yiyoung Liu <yiyoung.md@gmail.com>
2026-04-08 00:10:04 -0400

    feat: global colors and font

--- apps/frontend/src/routes/+layout.svelte
+++ apps/frontend/src/routes/+layout.svelte
@@ -5,5 +5,28 @@
 	let { children } = $props();
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Geist+Mono:wght@300..700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
 {@render children()}
+
+<style>
+	:global(:root) {
+		--color-white: #ffffff;
+		--color-gray: #f0f4f8;
+	}
+
+	:global(body) {
+		margin: 0;
+		padding: 0;
+
+		font-family: 'Inter', sans-serif;
+	}
+</style>
--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<link rel="icon" href="/favicon.ico" />
-<h1>CMU Courses</h1>
+<main>
+	<h1>CMU Courses</h1>
+</main>
