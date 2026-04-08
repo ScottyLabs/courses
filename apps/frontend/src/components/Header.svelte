8efb9e956879d10d13e6c3b48708db05183c019c
Yiyoung Liu <yiyoung.md@gmail.com>
2026-04-08 00:33:10 -0400

    feat: more navbar stuff

--- apps/frontend/src/components/Header.svelte
+++ apps/frontend/src/components/Header.svelte
@@ -1,3 +1,12 @@
-<header class="min-h-20 bg-gray shadow-md">
-	<div>Hi</div>
+<script>
+	import favicon from '@/lib/assets/favicon.ico';
+</script>
+
+<header class="flex min-h-20 items-center bg-gray shadow-md">
+	<a href="/">
+		<div class="flex items-center">
+			<img src={favicon} class="h-8 w-8 rounded" alt="favicon" />
+			<span class="ml-2">CMU Courses</span>
+		</div>
+	</a>
 </header>
