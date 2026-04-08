d08cf295cb998dfdee9a3920a5e922e0284bdda2
Yiyoung Liu <yiyoung.md@gmail.com>
2026-04-08 12:19:04 -0400

    feat: match header closer to figma

--- apps/frontend/src/lib/components/Header.svelte
+++ apps/frontend/src/lib/components/Header.svelte
@@ -1,33 +1,33 @@
 <script>
 	import { Icon } from '@steeze-ui/svelte-icon';
-	import { MagnifyingGlass, UserCircle, Clock } from '@steeze-ui/heroicons';
+	import { MagnifyingGlass, UserCircle, Clock, User } from '@steeze-ui/heroicons';
 	import favicon from '@/lib/assets/favicon.ico';
 </script>
 
-<header class="flex h-16 flex-row items-center justify-between bg-bg-secondary p-6 shadow-md">
+<header class="flex min-h-16 flex-row items-center justify-between bg-bg-secondary p-6 shadow-md">
 	<a href="/" class="flex flex-initial flex-row justify-start hover:underline">
-		<div class="flex items-center">
+		<div class="flex items-center gap-3">
 			<img src={favicon} class="h-8 w-8 rounded" alt="favicon" />
-			<span class="ml-2 font-medium text-fg-black">CMU Courses</span>
+			<span class="font-medium text-fg-black">CMU Courses</span>
 		</div>
 	</a>
 
-	<div class="flex flex-row items-center justify-between gap-6">
-		<a href="/courses" class="flex flex-row items-center">
-			<Icon src={MagnifyingGlass} class="h-5 w-5 [stroke-width:2.5] text-fg-gray" />
-			<span class="ml-2 font-medium text-fg-gray">Courses</span>
+	<div class="flex flex-row items-center justify-between gap-8">
+		<a href="/courses" class="flex flex-row items-center gap-2.5">
+			<Icon src={MagnifyingGlass} class="h-6 w-6 [stroke-width:2] text-fg-gray" />
+			<span class="font-medium text-fg-gray">Courses</span>
 		</a>
-		<a href="/courses" class="flex flex-row items-center">
-			<Icon src={UserCircle} class="h-5 w-5 [stroke-width:2.5] text-fg-gray" />
-			<span class="ml-2 font-medium text-fg-gray">Instructors</span>
+		<a href="/courses" class="flex flex-row items-center gap-2.5">
+			<Icon src={UserCircle} class="h-6 w-6 [stroke-width:2] text-fg-gray" />
+			<span class="font-medium text-fg-gray">Instructors</span>
 		</a>
-		<a href="/courses" class="flex flex-row items-center">
-			<Icon src={Clock} class="h-5 w-5 [stroke-width:2.5] text-fg-gray" />
-			<span class="ml-2 font-medium text-fg-gray">Schedules</span>
+		<a href="/courses" class="flex flex-row items-center gap-2.5">
+			<Icon src={Clock} class="h-6 w-6 [stroke-width:2] text-fg-gray" />
+			<span class="font-medium text-fg-gray">Schedules</span>
 		</a>
-		<a href="/courses" class="flex flex-row items-center">
-			<Icon src={UserCircle} class="h-5 w-5 [stroke-width:2.5] text-fg-gray" />
-			<span class="ml-2 font-medium text-fg-gray">Profile</span>
+		<a href="/courses" class="flex flex-row items-center gap-2.5">
+			<Icon src={User} class="h-6 w-6 [stroke-width:2] text-fg-gray" />
+			<span class="font-medium text-fg-gray">Profile</span>
 		</a>
 	</div>
 
