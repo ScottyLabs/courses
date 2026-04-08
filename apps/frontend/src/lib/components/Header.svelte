1919aed9f8e68b2973978f19bb5adfe2e4d69848
Yiyoung Liu <yiyoung.md@gmail.com>
2026-04-08 11:03:25 -0400

    feat: add more icons

--- apps/frontend/src/lib/components/Header.svelte
+++ apps/frontend/src/lib/components/Header.svelte
@@ -1,21 +1,33 @@
 <script>
 	import { Icon } from '@steeze-ui/svelte-icon';
-	import { MagnifyingGlass } from '@steeze-ui/heroicons';
+	import { MagnifyingGlass, UserCircle, Clock } from '@steeze-ui/heroicons';
 	import favicon from '@/lib/assets/favicon.ico';
 </script>
 
-<header class="flex h-16 flex-row items-center justify-between bg-gray p-6 shadow-md">
-	<a href="/" class="text-dark-gray-500 flex flex-initial flex-row justify-start hover:underline">
+<header class="bg-bg-gray flex h-16 flex-row items-center justify-between p-6 shadow-md">
+	<a href="/" class="flex flex-initial flex-row justify-start hover:underline">
 		<div class="flex items-center">
 			<img src={favicon} class="h-8 w-8 rounded" alt="favicon" />
-			<h6 class="ml-2 font-medium text-dark-gray">CMU Courses</h6>
+			<span class="ml-2 font-medium text-fg-black">CMU Courses</span>
 		</div>
 	</a>
 
-	<div>
+	<div class="flex flex-row items-center justify-between gap-6">
 		<a href="/courses" class="flex flex-row items-center">
-			<Icon src={MagnifyingGlass} class="h-5 w-5" />
-			<h6 class="ml-2 font-medium text-dark-gray">Courses</h6>
+			<Icon src={MagnifyingGlass} class="h-5 w-5 [stroke-width:2.5] text-fg-gray" />
+			<span class="ml-2 font-medium text-fg-gray">Courses</span>
+		</a>
+		<a href="/courses" class="flex flex-row items-center">
+			<Icon src={UserCircle} class="h-5 w-5 [stroke-width:2.5] text-fg-gray" />
+			<span class="ml-2 font-medium text-fg-gray">Instructors</span>
+		</a>
+		<a href="/courses" class="flex flex-row items-center">
+			<Icon src={Clock} class="h-5 w-5 [stroke-width:2.5] text-fg-gray" />
+			<span class="ml-2 font-medium text-fg-gray">Schedules</span>
+		</a>
+		<a href="/courses" class="flex flex-row items-center">
+			<Icon src={UserCircle} class="h-5 w-5 [stroke-width:2.5] text-fg-gray" />
+			<span class="ml-2 font-medium text-fg-gray">Profile</span>
 		</a>
 	</div>
 
