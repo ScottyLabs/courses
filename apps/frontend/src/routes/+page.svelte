5b69b39743ad9faa892f9fdd5f75e0b5ee322d55
Yiyoung <yiyoung.md@gmail.com>
2026-04-10 15:33:08 -0400

    feat: add the profile route

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import Header from '@/lib/components/Header.svelte';
 	import Footer from '@/lib/components/Footer.svelte';
-	import ProfileSidebar from '@/lib/components/ProfileSidebar.svelte';
 </script>
 
 <Header />
 <main class="flex flex-1 flex-col">
 	<div class="flex flex-1 flex-row">
-		<ProfileSidebar />
+        <!-- Auto redirect from here? -->
 	</div>
 </main>
 <Footer />
