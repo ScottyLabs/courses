40e1b30f538af07daaa08af3c62502b8a6dd6cec
Yiyoung <yiyoung.md@gmail.com>
2026-04-10 18:44:14 -0400

    feat: error page

---
+++ apps/frontend/src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<div class="flex flex-1 flex-col justify-center">
+	<div class="flex flex-row justify-center">
+		<div class="flex flex-col items-center text-fg-gray">
+			<span>Page Not Found</span>
+			<a href="/" class="border-1 m-2 p-2 rounded-md cursor-pointer">Go Back</a>
+		</div>
+	</div>
+</div>
