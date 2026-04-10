a253322f457ba31fef71ab8bb5e2323ad0f25a49
Yiyoung <yiyoung.md@gmail.com>
2026-04-10 18:52:29 -0400

    feat: add routes to profile sidebar

--- apps/frontend/src/routes/+error.svelte
+++ apps/frontend/src/routes/+error.svelte
@@ -2,7 +2,7 @@
 	<div class="flex flex-row justify-center">
 		<div class="flex flex-col items-center text-fg-gray">
 			<span>Page Not Found</span>
-			<a href="/" class="border-1 m-2 p-2 rounded-md cursor-pointer">Go Back</a>
+			<a href="/" class="m-2 cursor-pointer rounded-md border-1 p-2">Go Back</a>
 		</div>
 	</div>
 </div>
