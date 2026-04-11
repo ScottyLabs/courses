b4479443766b1f750ca79e523e0d3bf03dbc0a78
Yiyoung <yiyoung.md@gmail.com>
2026-04-10 21:06:08 -0400

    feat: draft of saved courses

---
+++ apps/frontend/src/routes/profile/saved/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import Header from '@/lib/components/Header.svelte';
+	import Footer from '@/lib/components/Footer.svelte';
+	import ProfileSidebar from '@/lib/components/ProfileSidebar.svelte';
+</script>
+
+<Header />
+<main class="flex flex-1 flex-col">
+	<div class="flex flex-1 flex-row">
+		<ProfileSidebar />
+		<div class="flex flex-col">
+			<div>
+				<div>Saved Courses</div>
+				<div>
+					<div>Deparment</div>
+					<div>Course Level</div>
+				</div>
+			</div>
+			<div>bottom</div>
+		</div>
+	</div>
+</main>
+<Footer />
