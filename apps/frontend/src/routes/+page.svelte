2e5929490f96b4c0b37b5cb8f6dfb2d008506721
Yiyoung Liu <yiyoung.md@gmail.com>
2026-04-12 00:24:39 -0400

    feat: add header and footer to layout

--- apps/frontend/src/routes/+page.svelte
+++ apps/frontend/src/routes/+page.svelte
@@ -1,12 +1,5 @@
-<script lang="ts">
-	import Header from '@/lib/components/Header.svelte';
-	import Footer from '@/lib/components/Footer.svelte';
-</script>
-
-<Header />
 <main class="flex flex-1 flex-col">
 	<div class="flex flex-1 flex-row">
 		<!-- Auto redirect from here? -->
 	</div>
 </main>
-<Footer />
